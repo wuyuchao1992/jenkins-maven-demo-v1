5b9eb2e448e42efa9fde9eb2cdc10624c1441caf
tony <550535582@qq.com>
2023-03-25 18:13:53 +0800

    update project

---
+++ server/src/main/java/com/example/Person.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Person {
+
+  /**
+   * This is a constructor.
+   */
+  public Person() {
+
+  }
+
+  /**
+   * @param someone 是人名
+   * @return 字符串
+   */
+  public final String person(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
---
+++ webapp/src/main/webapp/index.jsp
@@ -0,0 +1,1 @@
+Hello, Welcome to this course! Pipeline as code section for AWS. updated
