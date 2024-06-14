373b94bc2af601b7e94b28121fcb6893870160fd
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-06-14 20:37:46 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,7 +1,7 @@
 import { runWithContext } from '../context';
 
-export function withContext(stepFunction: () => void | Promise<void>) {
-  return function (...args: any[]) {
+export function withContext<T extends (...args: any[]) => any>(stepFunction: T): T {
+  return function (this: any, ...args: any[]) {
     return runWithContext(() => stepFunction.apply(this, args));
-  };
+  } as T;
 }
