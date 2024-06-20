8bd4df06625d043078c0fda36450d0bec85b65e3
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-06-20 16:24:14 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,12 +1,13 @@
- if (ulDivHandle) {
-    // 获取 div.ul.label 元素的 test-id 属性值
-    const labels = await ulDivHandle.evaluate(ulDiv => {
-      const labelElements = ulDiv.querySelectorAll('.label');
-      return Array.from(labelElements).map(label => label.getAttribute('test-id'));
-    });
-
-    // 输出每个 test-id 的值
-    console.log(labels);
-  } else {
-    console.error('Could not find the div.ul element.');
-  }
+if (ulDivHandle) {
+  // 获取 div.ul.label 元素的 test-id 属性值
+  const labels = await ulDivHandle.evaluate(ulDiv => {
+    if (!ulDiv) {
+      return []; // 如果 ulDiv 为 null，则返回空数组
+    }
+    const labelElements = ulDiv.querySelectorAll('.label');
+    return Array.from(labelElements).map(label => label.getAttribute('test-id'));
+  });
+  console.log(labels);
+} else {
+  console.error('Could not find the div.ul element.');
+}
