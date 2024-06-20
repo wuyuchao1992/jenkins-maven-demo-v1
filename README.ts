9325d7fc9f4138a9e89f7240fbd22da6b845b079
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-06-20 16:10:39 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,11 +1,12 @@
-  // 根据按钮定位到相关的 div.ul 元素
-  const ulDivHandle = await button.evaluateHandle(button => {
-    // 在 button 的祖先元素中找到相应的 div.ul
-    return button.closest('some-parent-selector').querySelector('div.ul');
-  });
+ if (ulDivHandle) {
+    // 获取 div.ul.label 元素的 test-id 属性值
+    const labels = await ulDivHandle.evaluate(ulDiv => {
+      const labelElements = ulDiv.querySelectorAll('.label');
+      return Array.from(labelElements).map(label => label.getAttribute('test-id'));
+    });
 
-  // 获取 div.ul.label 元素的 test-id 属性值
-  const labels = await ulDivHandle.evaluate(ulDiv => {
-    const labelElements = ulDiv.querySelectorAll('.label');
-    return Array.from(labelElements).map(label => label.getAttribute('test-id'));
-  });
+    // 输出每个 test-id 的值
+    console.log(labels);
+  } else {
+    console.error('Could not find the div.ul element.');
+  }
