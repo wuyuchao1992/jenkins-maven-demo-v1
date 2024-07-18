21bc10dee082aeb7e260113f8801506cd14ed659
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 20:33:15 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,6 +1,12 @@
+import { Given } from '@cucumber/cucumber';
+
 Given('I have the following key-value pairs:', function (dataTable) {
   const fieldsToPrint: Record<string, any> = {};
-  dataTable.rawTable.slice(1).forEach(([key, value]: KeyValuePairs[]) => {
+  dataTable.rawTable.slice(1).forEach(([key, value]) => {
     fieldsToPrint[key] = value;
   });
   console.log('Key-Value Pairs:');
+  for (const [key, value] of Object.entries(fieldsToPrint)) {
+    console.log(`Key: ${key}, Value: ${value}`);
+  }
+});
