09e1dbc0b540f2cffdd5e42e6b0c8d580ad2da9b
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 18:17:41 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,16 @@
-        const value = (Object.keys(fields[fieldStr]).length === 0 && fields[fieldStr].constructor === Object) ? {} : fields[fieldStr];
+import { Given } from '@cucumber/cucumber';
+import { DataTable } from '@cucumber/cucumber';
+
+Given('I have the following key-value pairs:', function (this: any, dataTable: DataTable) {
+  const fieldsToUpdate: Record<string, any> = {};
+
+  // Ensure TypeScript knows the structure of rawTable
+  const rawTable = dataTable.raw().slice(1);
+
+  rawTable.forEach(([key, value]) => {
+    // Parse the value as JSON
+    fieldsToUpdate[key] = JSON.parse(value);
+  });
+
+  console.log('Fields to Update:', fieldsToUpdate);
+});
