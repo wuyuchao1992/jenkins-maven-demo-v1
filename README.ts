3c467de67ffe83a0c4e49aec6e14b0be16a6d3d3
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 22:03:52 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,10 +1,1 @@
-function processKeyValuePairs(fields: { [key: string]: any }) {
-  Object.keys(fields).forEach(key => {
-    // 如果字段是一个空对象，则设置为空字符串
-    if (Object.keys(fields[key]).length === 0 && fields[key].constructor === Object) {
-      set(obj, key, '');
-    } else {
-      set(obj, key, fields[key]);
-    }
-  });
-}
+        const value = (Object.keys(fields[fieldStr]).length === 0 && fields[fieldStr].constructor === Object) ? {} : fields[fieldStr];
