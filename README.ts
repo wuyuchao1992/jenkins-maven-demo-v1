2aa75c9ea50e6ee787820f943c1cf98603b1b5bd
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 22:45:19 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,20 @@
-'--max-old-space-size=4096' // 增加到 4GB
+ args: [
+      '--disable-extensions',
+      '--disable-gpu',
+      '--disable-software-rasterizer',
+      '--single-process',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--no-zygote',
+      '--disable-default-apps',
+      '--disable-webgl',
+      '--disable-notifications',
+      '--disable-background-networking',
+      '--max-old-space-size=4096', // 增加内存限制
+      '--disable-gpu-compositing',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-renderer-backgrounding',
+      '--disable-plugins'
+    ]
