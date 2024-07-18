8fa336578a10e9cdb0eb6158f866ea239ba5dbc3
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 22:38:26 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,16 +1,6 @@
-import { Given } from '@cucumber/cucumber';
-import { DataTable } from '@cucumber/cucumber';
-
-Given('I have the following key-value pairs:', function (this: any, dataTable: DataTable) {
-  const fieldsToUpdate: Record<string, any> = {};
-
-  // Ensure TypeScript knows the structure of rawTable
-  const rawTable = dataTable.raw().slice(1);
-
-  rawTable.forEach(([key, value]) => {
-    // Parse the value as JSON
-    fieldsToUpdate[key] = JSON.parse(value);
+    args: [
+      '--disable-dev-shm-usage',
+      '--disable-background-timer-throttling',
+      '--no-sandbox', // 可选项，如果在 CI/CD 环境中运行
+    ]
   });
-
-  console.log('Fields to Update:', fieldsToUpdate);
-});
