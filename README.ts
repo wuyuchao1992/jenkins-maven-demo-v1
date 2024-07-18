f49207365d6e9c71fa8ea7ee6d7d7219b4ddfbfd
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-18 22:51:13 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,20 +1,2 @@
- args: [
-      '--disable-extensions',
-      '--disable-gpu',
-      '--disable-software-rasterizer',
-      '--single-process',
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--no-zygote',
-      '--disable-default-apps',
-      '--disable-webgl',
-      '--disable-notifications',
-      '--disable-background-networking',
-      '--max-old-space-size=4096', // 增加内存限制
-      '--disable-gpu-compositing',
-      '--disable-background-timer-throttling',
-      '--disable-backgrounding-occluded-windows',
-      '--disable-renderer-backgrounding',
-      '--disable-plugins'
-    ]
+   javaScriptEnabled: true,
+    ignoreHTTPSErrors: true,
