aac9774c5079fa19e852cc09146e7e5db12c064b
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-25 22:27:09 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,1 @@
-    expect(fileStatus).to.equal(expectedStatus, `File status did not become ${expectedStatus} within the allowed attempts.`);
+    expect(fileStatus).toBe(expectedStatus, `File status did not become ${expectedStatus} within the allowed attempts.`);
