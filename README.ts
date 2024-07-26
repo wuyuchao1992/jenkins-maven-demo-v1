9f3e438d4950acecf10fe7b849e61f78ff8a5a11
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-26 21:15:46 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -2,3 +2,7 @@
         MY_SECRET_TEXT = credentials('my-secret-text-id') // Replace with your actual credentials ID
         MY_USERNAME_PASSWORD = credentials('my-username-password-id')
     }
+
+         echo "Using secret text: ${env.MY_SECRET_TEXT}"
+                    echo "Using username: ${env.MY_USERNAME_PASSWORD_USR}"
+                    echo "Using password: ${env.MY_USERNAME_PASSWORD_PSW}"
