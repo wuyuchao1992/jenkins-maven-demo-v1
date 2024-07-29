4f4ba7b48bbdea61bf083f7213fa5fb848d7482c
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-29 11:42:57 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,8 +1,4 @@
- environment {
-        MY_SECRET_TEXT = credentials('my-secret-text-id') // Replace with your actual credentials ID
-        MY_USERNAME_PASSWORD = credentials('my-username-password-id')
-    }
-
-         echo "Using secret text: ${env.MY_SECRET_TEXT}"
-                    echo "Using username: ${env.MY_USERNAME_PASSWORD_USR}"
-                    echo "Using password: ${env.MY_USERNAME_PASSWORD_PSW}"
+withCredentials([usernamePassword(credentialsId: 'my-credentials-id', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    sh 'echo $USERNAME'
+    sh 'echo $PASSWORD'
+}
