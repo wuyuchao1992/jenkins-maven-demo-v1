621413cd7e65f29c42dba24b625ed76edd17e48f
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-31 19:27:00 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -6,3 +6,25 @@ When('I intercept API requests to {string}', async function (apiEndpoint: string
     await route.fulfill({ response });
   });
 });
+
+class CustomWorld extends World {
+  browser: Browser;
+  page: Page;
+  responses: any[];
+
+  constructor(options) {
+    super(options);
+    this.responses = [];
+  }
+
+  async openBrowser() {
+    this.browser = await chromium.launch();
+    this.page = await this.browser.newPage();
+  }
+
+  async closeBrowser() {
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
