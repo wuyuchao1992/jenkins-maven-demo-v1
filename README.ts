905b66b09a69bc1eb3c66d1873765637d2d3c4db
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-07-31 19:26:13 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,8 @@
-withCredentials([usernamePassword(credentialsId: 'my-credentials-id', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-    sh 'echo $USERNAME'
-    sh 'echo $PASSWORD'
-}
+When('I intercept API requests to {string}', async function (apiEndpoint: string) {
+  await this.page.route(apiEndpoint, async (route, request) => {
+    const response = await this.page.request.fetch(request);
+    const jsonResponse = await response.json();
+    this.responses.push(jsonResponse);
+    await route.fulfill({ response });
+  });
+});
