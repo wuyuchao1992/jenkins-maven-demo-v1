7640ec016db24db4289b548b1f7779e53d8b8bba
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-06 20:28:35 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,25 +1,21 @@
-import { Before, After } from 'cucumber';
+import { BeforeAll, AfterAll, setDefaultTimeout } from 'cucumber';
 import { browser } from 'playwright';
 
-let singleThreadFeatureRunning = false;
+let isSingleThreadRunning = false;
+let waitingQueue: (() => void)[] = [];
 
-Before({ tags: '@singleThread' }, async function () {
-  // 假设只有一个浏览器实例在跑单线程的feature
-  if (singleThreadFeatureRunning) {
-    // 阻塞其他的feature，直到单线程的feature完成
-    await new Promise(resolve => {
-      const check = setInterval(() => {
-        if (!singleThreadFeatureRunning) {
-          clearInterval(check);
-          resolve(true);
-        }
-      }, 100);
-    });
+BeforeAll({ tags: '@singleThread' }, async function () {
+  if (isSingleThreadRunning) {
+    await new Promise<void>(resolve => waitingQueue.push(resolve));
   } else {
-    singleThreadFeatureRunning = true;
+    isSingleThreadRunning = true;
   }
 });
 
-After({ tags: '@singleThread' }, async function () {
-  singleThreadFeatureRunning = false;
+AfterAll({ tags: '@singleThread' }, async function () {
+  isSingleThreadRunning = false;
+  if (waitingQueue.length > 0) {
+    const next = waitingQueue.shift();
+    if (next) next();
+  }
 });
