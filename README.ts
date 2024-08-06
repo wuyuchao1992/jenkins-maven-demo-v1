042a248d7cb7b88e381a9f9c82694a6330e99024
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-06 19:54:56 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,11 +1,9 @@
-// hooks.ts
-import { setParallelCanAssign, ITestCaseHookParameter } from '@cucumber/cucumber';
+import { setParallelCanAssign } from '@cucumber/cucumber';
 
-setParallelCanAssign((testCase: ITestCaseHookParameter) => {
-  // 检查是否有 @singleThread 标签
-  const tags = testCase.pickle.tags.map(tag => tag.name);
-  if (tags.includes('@singleThread')) {
-    return false; // 禁止并行执行
+setParallelCanAssign((type, scenario) => {
+  // 检查场景是否标记为单线程
+  if (scenario.tags.find(tag => tag.name === '@singleThread')) {
+    return false; // 如果标记为@singleThread，则不并行运行
   }
-  return true; // 允许并行执行
+  return true; // 其他情况下并行运行
 });
