3ce8ab7246522b6b7f1eac55cf547683ea60c24b
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-06 19:59:36 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,10 @@
 import { setParallelCanAssign } from '@cucumber/cucumber';
 
 setParallelCanAssign((type, scenario) => {
-  // 检查场景是否标记为单线程
-  if (scenario.tags.find(tag => tag.name === '@singleThread')) {
-    return false; // 如果标记为@singleThread，则不并行运行
+  // 检查 scenario 对象中的 tags 属性是否包含 @singleThread 标签
+  const tags = scenario.pickle.tags;
+  if (tags.some(tag => tag.name === '@singleThread')) {
+    return false; // 如果标记为 @singleThread，则不并行运行
   }
   return true; // 其他情况下并行运行
 });
