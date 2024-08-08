6ae98aa5b99127c4e930cfdff2b0ea75e0ff4b92
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-08 22:54:52 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -37,41 +37,11 @@ reporter.generate({
 
 console.log('HTML report has been generated at', outputHtmlFile);
 
-const fs = require('fs');
-const path = require('path');
-const merge = require('cucumber-json-merge');
-const reporter = require('multiple-cucumber-html-reporter');
-
-const jsonDir = 'reports';
-const outputJsonFile = path.join(jsonDir, 'cucumber-combined.json');
-const outputHtmlFile = 'report/cucumber-report.html'; // Place the report in the report directory
-
-// Get all JSON files
-const jsonFiles = fs.readdirSync(jsonDir).filter(file => file.endsWith('.json')).map(file => path.join(jsonDir, file));
-
-// Merge JSON files into one
-const combinedReport = merge(jsonFiles);
-
-// Write the merged JSON file
-fs.writeFileSync(outputJsonFile, JSON.stringify(combinedReport, null, 2));
-
-// Generate HTML report
-reporter.generate({
-  jsonDir: jsonDir,
-  reportPath: outputHtmlFile,
-  metadata: {
-    browser: {
-      name: 'chrome',
-      version: '89.0.4389.82'
-    },
-    device: 'Local test machine',
-    platform: {
-      name: 'Windows',
-      version: '10'
-    }
-  },
-  displayReportTime: true,
-  openReportInBrowser: true
-});
-
-console.log('HTML report has been generated at', outputHtmlFile);
+{
+  "scripts": {
+    "test:singleThread": "cucumber-js --profile singleThread",
+    "test:parallel": "cucumber-js --profile default",
+    "merge-and-generate-report": "node merge-and-generate-report.js",
+    "test:all": "npm run prepare-reports && npm run test:singleThread && npm run test:parallel && npm run merge-and-generate-report"
+  }
+}
