3b6c66af42c22693edf075f488781f33d4791dbf
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-08 21:31:44 +1200

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,21 +1,32 @@
-npm install multiple-cucumber-html-reporter cucumber-json-merge concurrently --save-dev
+npm install typescript ts-node @types/node multiple-cucumber-html-reporter cucumber-json-merge concurrently --save-dev
 
-module.exports = {
-  default: {
-    format: ['pretty', 'json:reports/cucumber-parallel.json'],
-    require: ['features/**/*.ts'],
-    tags: 'not @singleThread',
-    parallel: 6
+import { generate } from 'multiple-cucumber-html-reporter';
+
+generate({
+  jsonDir: 'reports',
+  reportPath: 'reports/html',
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: '89'
+    },
+    device: 'Local test machine',
+    platform: {
+      name: 'ubuntu',
+      version: '20.04'
+    }
   },
-  singleThread: {
-    format: ['pretty', 'json:reports/cucumber-single-thread.json'],
-    require: ['features/**/*.ts'],
-    tags: '@singleThread',
-    parallel: 1
+  customData: {
+    title: 'Run info',
+    data: [
+      { label: 'Project', value: 'My Project' },
+      { label: 'Release', value: '1.2.3' },
+      { label: 'Cycle', value: 'B11221.34321' },
+      { label: 'Execution Start Time', value: new Date().toISOString() },
+      { label: 'Execution End Time', value: new Date().toISOString() }
+    ]
   }
-};
-npm install multiple-cucumber-html-reporter cucumber-json-merge concurrently --save-dev
-
+});
 
 {
   "scripts": {
@@ -23,8 +34,7 @@ npm install multiple-cucumber-html-reporter cucumber-json-merge concurrently --s
     "test:parallel": "cucumber-js",
     "test:singleThread": "cucumber-js --profile singleThread",
     "merge-reports": "cucumber-json-merge reports/cucumber-parallel.json reports/cucumber-single-thread.json > reports/cucumber-combined.json",
-    "generate-report": "multiple-cucumber-html-reporter --report-dir reports/html --json-dir reports",
+    "generate-report": "ts-node generate-html-report.ts",
     "test:all": "npm run prepare-reports && concurrently \"npm run test:singleThread\" \"npm run test:parallel\" && npm run merge-reports && npm run generate-report"
   }
 }
-
