fd9da9ed69673683dc9b0eb2504eedf4f307c753
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-14 12:05:56 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,2 @@
-import { After, Status } from '@cucumber/cucumber';
-import { Page } from 'playwright';
-
-After(async function (this: { page: Page }, scenario) {
-    if (scenario.result?.status === Status.FAILED) {
-        const screenshot = await this.page.screenshot({ path: `reports/screenshots/${scenario.pickle.name}-${Date.now()}.png`, fullPage: true });
-        this.attach(screenshot, 'image/png');
-    }
-});
+npx cucumber-js --format json:reports/cucumber_report.json
+npx cucumber-html-reporter --theme bootstrap --jsonFile reports/cucumber_report.json --output reports/cucumber_report.html --screenshotsDirectory reports/screenshots/
