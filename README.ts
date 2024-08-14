2f61af9322a2e8d3410fe3badcbf409bc3f4b8aa
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-08-14 12:11:14 +0800

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,2 +1,15 @@
-npx cucumber-js --format json:reports/cucumber_report.json
-npx cucumber-html-reporter --theme bootstrap --jsonFile reports/cucumber_report.json --output reports/cucumber_report.html --screenshotsDirectory reports/screenshots/
+import { BeforeAll } from '@cucumber/cucumber';
+import fs from 'fs';
+import path from 'path';
+
+BeforeAll(() => {
+    const screenshotDir = path.join(__dirname, '..', 'reports', 'screenshots');
+
+    // 如果截图目录存在，删除其中的所有文件
+    if (fs.existsSync(screenshotDir)) {
+        fs.readdirSync(screenshotDir).forEach(file => {
+            const filePath = path.join(screenshotDir, file);
+            fs.unlinkSync(filePath);
+        });
+    }
+});
