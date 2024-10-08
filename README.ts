ff04938ff74de0db2e603e599ff469ce4630be7f
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-10-09 00:30:28 +1300

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,8 +1,3 @@
-import org.apache.jmeter.functions.FunctionUtils
-import org.apache.jmeter.engine.util.CompoundVariable
-
-// 调用 __RandomString 函数生成 5 位随机字符串
-def randomStringFunction = new CompoundVariable("__RandomString(5,abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789)").execute()
-
-// 将随机字符串存储为 JMeter 变量
-vars.put("randomString", randomStringFunction)
+def charset = ('A'..'Z' + 'a'..'z' + '0'..'9').join()
+def randomString = (1..5).collect { charset[new Random().nextInt(charset.length())] }.join()
+vars.put("randomString", randomString)
