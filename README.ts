2ce7f077e3e1639088256ca7d40315b5e0cacec5
wuyuchao1992 <45410926+wuyuchao1992@users.noreply.github.com>
2024-11-07 15:45:40 +1300

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,23 +1,6 @@
-<dependency>
-    <groupId>io.github.bonigarcia</groupId>
-    <artifactId>webdrivermanager</artifactId>
-    <version>5.3.2</version> 
-</dependency>
+<properties>
+    <chromedriver.path>${project.basedir}/drivers/chromedriver</chromedriver.path>
+</properties>
 
 
-import io.github.bonigarcia.wdm.WebDriverManager
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-
-// 使用 WebDriverManager 自动配置 chromedriver
-WDS.sampleResult.sampleStart()
-WebDriverManager.chromedriver().setup()
-
-ChromeOptions options = new ChromeOptions()
-options.addArguments("--headless") // 在 CI/CD 中运行无界面模式
-options.addArguments("--no-sandbox")
-options.addArguments("--disable-dev-shm-usage")
-
-WDS.browser = new ChromeDriver(options)
-WDS.browser.get("https://example.com")
-WDS.sampleResult.sampleEnd()
+${__P(chromedriver.path)}
